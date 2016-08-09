fab2d326c441a60d0edec06da15486db6165c220
dwrf6 <info@donnyfairborn.com>
2016-08-09 01:07:30 -0400

    set center spacing with flexbox

--- app/routes/cyclists.js
+++ app/routes/cyclists.js
@@ -2,6 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model() {
-		return ['Eddy Merckx', 'Gary Fisher', 'Greg LeMond', 'Tullio Campagnolo', 'Fausto Coppi'];
+		return [
+		'Eddy Merckx', 
+		'Gary Fisher',
+		'Greg LeMond', 
+		'Tullio Campagnolo', 
+		'Fausto Coppi'
+		]
 	}
 });
